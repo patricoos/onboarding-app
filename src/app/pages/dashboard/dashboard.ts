fcb77c011859ec2c4332bec8d3ee7b8e460e798a
Patryk Bujalla <patryk.bujalla@intechnity.com>
2025-07-28 23:14:47 +0200

    read

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -7,12 +7,17 @@ import { Component, inject, OnInit } from '@angular/core';
     template: ` <div class="grid grid-cols-12 gap-8">Hello</div> `
 })
 export class Dashboard implements OnInit {
- private firestoreService = inject(FirestoreService);
-  users: any[] = [];
+    private firestoreService = inject(FirestoreService);
+    users: any[] = [];
 
     ngOnInit() {
-        this.firestoreService.getUsers().subscribe((data) => {
-            this.users = data;
+        this.firestoreService.getUsers().subscribe({
+            next: (data) => {
+                this.users = data;
+                console.log(data);
+                
+            },
+            error: (error) => console.error(error)
         });
     }
 }
--- src/app/shared/services/firestore-service.ts
+++ src/app/shared/services/firestore-service.ts
@@ -1,6 +1,6 @@
 // firestore.service.ts
 import { inject, Injectable } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { CollectionReference, DocumentData, Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
@@ -8,7 +8,7 @@ export class FirestoreService {
  private firestore = inject(Firestore);
 
   getUsers(): Observable<any[]> {
-    const usersRef = collection(this.firestore, 'users');
+    const usersRef: CollectionReference<DocumentData> = collection(this.firestore, 'users');
     return collectionData(usersRef, { idField: 'id' });
   }
 }
