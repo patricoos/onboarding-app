68d60e1d064673ba0896b291df434fc1f940bd72
Patryk Bujalla <patryk.bujalla@intechnity.com>
2025-07-28 23:05:37 +0200

    update

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { FirestoreService } from '@/shared/services/firestore-service';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-dashboard',
     imports: [],
-    template: `
-        <div class="grid grid-cols-12 gap-8">
-            Hello 
-        </div>
-    `
+    template: ` <div class="grid grid-cols-12 gap-8">Hello</div> `
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+ private firestoreService = inject(FirestoreService);
+  users: any[] = [];
+
+    ngOnInit() {
+        this.firestoreService.getUsers().subscribe((data) => {
+            this.users = data;
+        });
+    }
+}
--- src/app/shared/services/firestore-service.ts
+++ src/app/shared/services/firestore-service.ts
@@ -1,11 +1,11 @@
 // firestore.service.ts
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class FirestoreService {
-  constructor(private firestore: Firestore) {}
+ private firestore = inject(Firestore);
 
   getUsers(): Observable<any[]> {
     const usersRef = collection(this.firestore, 'users');
